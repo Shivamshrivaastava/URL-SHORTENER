d10ea8f23e08bdae519c1a19cc9f90f4bec033c1
Shivamshrivaastava <SHIVAMSHRIVAASTAVA@GMAIL.COM>
2022-09-13 13:50:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-24ljfj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-24ljfj)
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import  React, { useState } from 'react';
+import './App.css';
+import InputShorten from './components/InputShorten';
+import LinkResult from './components/LinkResult';
+import Background from './components/Background';
+
+function App() {
+  const [inputValue, setInputValue] = useState("")
+
+  return (
+    <div className="container">
+      <Background/>
+      <InputShorten setInputValue={setInputValue} />
+      <LinkResult inputValue={inputValue} />
+    </div>
+  );
+}
+
+export default App;
